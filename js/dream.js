566ad1977252d0625e29262af0ed46984ff945d5
Jason Zou <jason.zou@nwayschina.com>
2015-10-21 13:43:53 +0800

    增加其他

--- js/dream.js
+++ js/dream.js
@@ -1,3 +1,61 @@
+window.requestNextAnimationFrame = (function () {
+	var originalWebkitRequestAnimationFrame = undefined,
+		wrapper = undefined,
+		callback = undefined,
+		geckoVersion = 0,
+		userAgent = navigator.userAgent,
+		index = 0,
+		self = this;
+	
+	if (window.webkitRequestAnimationFrame) {
+		wrapper = function (time) {
+		if (time === undefined) {
+			time = +new Date();
+		}
+		self.callback(time);
+		};
+		
+		originalWebkitRequestAnimationFrame = window.webkitRequestAnimationFrame;    
+
+		window.webkitRequestAnimationFrame = function (callback, element) {
+		self.callback = callback;
+		originalWebkitRequestAnimationFrame(wrapper, element);
+		}
+	}
+	if (window.mozRequestAnimationFrame) {
+		index = userAgent.indexOf('rv:');
+
+		if (userAgent.indexOf('Gecko') != -1) {
+		geckoVersion = userAgent.substr(index + 3, 3);
+
+		if (geckoVersion === '2.0') {
+			window.mozRequestAnimationFrame = undefined;
+		}
+		}
+	}
+	
+	return window.requestAnimationFrame   ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		window.oRequestAnimationFrame      ||
+		window.msRequestAnimationFrame     ||
+
+		function (callback, element) {
+		var start,
+			finish;
+
+		window.setTimeout( function () {
+			start = +new Date();
+			callback(start);
+			finish = +new Date();
+
+			self.timeout = 1000 / 60 - (finish - start);
+
+		}, self.timeout);
+		};
+	}
+)();
+
 zodream = function() {
 	return new zodream.fn();
 };
@@ -66,19 +124,88 @@ zodream.fn.prototype = {
 		this.scene.add(this.light);
 	},
 	initObject: function() {
+		this.sphere = new THREE.Mesh(
+				new THREE.SphereGeometry(20,20),                //width,height,depth
+				new THREE.MeshLambertMaterial({color: 0xff0000}) //材质设定
+		);
+		this.scene.add(this.sphere);
+		this.sphere.position.set(0,0,0);	
+	},
+	initLine: function() {
 		var geometry = new THREE.Geometry();
 		var material = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors} );
 		var color1 = new THREE.Color( 0x444444 ), color2 = new THREE.Color( 0xFF0000 );
-
+		
 		// 线的材质可以由2点的颜色决定
-		var p1 = new THREE.Vector3( -100, 0, 100 );
-		var p2 = new THREE.Vector3(  100, 0, -100 );
-		geometry.vertices.push(p1);
-		geometry.vertices.push(p2);
+		geometry.vertices.push(
+			new THREE.Vector3( -100, 0, 100 ),
+			new THREE.Vector3(  100, 0, -100 )
+		);
 		geometry.colors.push( color1, color2 );
-
-		var line = new THREE.Line( geometry, material, THREE.LineSegments );
+		var line = new THREE.Line( geometry, material, THREE.LineSegments );  //LineSegments一系列线
 		this.scene.add(line);
+	},
+	initBone: function() {
+		new THREE.Bone()	//骨头
+		SkinnedMesh          //骨骼
+		
+		
+		var bones = [];
+		
+		var shoulder = new THREE.Bone();
+		var elbow = new THREE.Bone();
+		var hand = new THREE.Bone();
+		
+		shoulder.add( elbow );
+		elbow.add( hand );
+		
+		bones.push( shoulder );
+		bones.push( elbow );
+		bones.push( hand );
+		
+		shoulder.position.y = -5;
+		elbow.position.y = 0;
+		hand.position.y = 5;
+		
+		var armSkeleton = THREE.Skeleton( bones );        //骨骼
+	},
+	initLensFlare: function() {
+		new THREE.LensFlare() //光晕	
+	},
+	initLOD: function() {
+		var lod = new THREE.LOD();             //显示水平网格
+		for( var i = 0; i < 5; i++ ) {
+			
+			var geometry = new THREE.IcosahedronGeometry( 10, 5 - i )
+			
+			new THREE.Mesh( geometry, material );
+			
+			lod.addLevel( mesh, i * 50 );
+			
+		}	
+	},
+	initMesh: function() {
+		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+		var mesh = new THREE.Mesh( geometry, material );             //网格对象的基类
+		scene.add( mesh );	
+	},
+	initMorphAnimMesh: function() {
+		meshAnim = new THREE.MorphAnimMesh( geometry, material );                //变形动画
+		meshAnim.duration = 1000;
+		scene.add( meshAnim );	
+		
+		var delta = clock.getDelta();
+		meshAnim.updateAnimation( 1000 * delta );
+	},
+	initPoints: function() {
+		Points                           //粒子
+	},
+	initSprite: function() {
+		var map = THREE.ImageUtils.loadTexture( "sprite.png" );
+		var material = new THREE.SpriteMaterial( { map: map, color: 0xffffff, fog: true } );
+		var sprite = new THREE.Sprite( material );
+		scene.add( sprite );	
 	},
 	initTween: function() {
 		new TWEEN.Tween( mesh.position)
@@ -88,7 +215,7 @@ zodream.fn.prototype = {
 	render: function() {
 		this.renderer.clear();
 		this.renderer.render(this.scene, this.camera);
-		requestAnimationFrame(this.render.bind(this));
+		window.requestNextAnimationFrame(this.render.bind(this));
 		
 		this.stats.update();
 	}
